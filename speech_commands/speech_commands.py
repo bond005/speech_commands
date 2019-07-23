c1932a181df03038c09ebf1f5115846b740fa462
Ivan Bondarenko <bond005@yandex.ru>
2019-07-23 18:10:06 +0700

    Early stopping procedure has been improved.

--- speech_commands/speech_commands.py
+++ speech_commands/speech_commands.py
@@ -140,7 +140,8 @@ class SoundRecognizer(ClassifierMixin, BaseEstimator):
                 cache_dir_name=self.cache_dir, suffix='valid'
             )
             early_stopping_callback = keras.callbacks.EarlyStopping(
-                patience=self.patience, verbose=self.verbose, restore_best_weights=True, monitor='val_loss', mode='min'
+                patience=self.patience, verbose=self.verbose, restore_best_weights=True,
+                monitor='val_categorical_accuracy', mode='min'
             )
             self.recognizer_.fit_generator(trainset_generator, validation_data=validset_generator, shuffle=True,
                                            epochs=self.max_epochs, verbose=2 if self.verbose else 0,
